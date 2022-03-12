06e16e8359bb74b859a016d648db6af03598f28c
nounous13 <inesbekara90@gmail.com>
2022-03-12 20:34:57 +0100

    first

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Star Admin2 </title>
+  <title>gestion scolarité</title>
   <!-- plugins:css -->
   <link rel="stylesheet" href="../../vendors/feather/feather.css">
   <link rel="stylesheet" href="../../vendors/mdi/css/materialdesignicons.min.css">
@@ -44,8 +44,8 @@
       <div class="navbar-menu-wrapper d-flex align-items-top"> 
         <ul class="navbar-nav">
           <li class="nav-item font-weight-semibold d-none d-lg-block ms-0">
-            <h1 class="welcome-text">Good Morning, <span class="text-black fw-bold">John Doe</span></h1>
-            <h3 class="welcome-sub-text">Your performance summary this week </h3>
+            <h1 class="welcome-text">welcome <span class="text-black fw-bold"></span></h1>
+            <h3 class="welcome-sub-text"></h3>
           </li>
         </ul>
         <ul class="navbar-nav ms-auto">
@@ -374,7 +374,7 @@
           <li class="nav-item">
             <a class="nav-link" href="../../index.html">
               <i class="mdi mdi-grid-large menu-icon"></i>
-              <span class="menu-title">Dashboard</span>
+              <span class="menu-title">statistique</span>
             </a>
           </li>
           <li class="nav-item nav-category">UI Elements</li>
